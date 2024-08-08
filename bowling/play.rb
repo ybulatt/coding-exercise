4452e53db99b498626f264e56306aed64be760db
Ivan Li <ili@financeit.io>
2024-08-08 16:54:36 -0400

    Release beta

--- bowling/play.rb
+++ bowling/play.rb
@@ -3,21 +3,78 @@
 class BowlingGame
   def initialize(total_frames: 10)
     @total_frames = total_frames
-    setup_game
+    @frames = []
+    @scores_per_frame = []
   end
 
   def start
-  end
+    total_frames.times do |index|
+      puts "Frame: #{index + 1}"
 
-  private
+      frame = []
 
-  def setup_game
-    @frames = []
-    (total_frames - 1).times do
-      @frames << { first: nil, second: nil, score: 0 }
+      2.times do
+        puts "Pins knocked down:"
+        pins = gets
+        frame << pins
+
+        break if pins&.downcase == 'x'?
+      end
+
+      extra_bowls = 0
+
+      if index == total_frames - 1
+        if frame.first&.downcase == 'x'
+          extra_bowls = 2
+        elsif frame.second == '/'
+          extra_bowls = 1
+        end
+      end
+
+      extra_bowls.times do
+        puts "Pins knocked down:"
+        pins = gets
+        frame << pins
+      end
+
+      @frames << frame
     end
 
-    @frames << { first: nil, second: nil, third: nil, score: 0 }
+    @frames.each_with_index do |frame, index|
+      puts "Frame: #{index + 1}"
+
+      total_score_this_frame = 0
+
+      if frame.include?('x') || frame.include?('X')
+        total_score_this_frame += 10
+        
+        break if index == @total_frames + 1
+
+        next_frame = @frames[index + 1]
+
+        if next_frame.include?('x') || next_frame.include?('X') || next_frame.include?('/')
+          total_score_this_frame += 10
+        else
+          total_score_this_frame += next_frame.map(&:to_i).sum
+        end
+      elsif frame.include?('/')
+        total_score_this_frame += 10
+
+        if next_frame.include?('x') || next_frame.include?('X') || next_frame.include?('/')
+          total_score_this_frame += 10
+        else
+          total_score_this_frame += next_frame.map(&:to_i).first
+        end
+      else
+        total_score_this_frame += frame.map(&:to_i).sum
+      end
+
+      total_score_this_frame += @scores_per_frame.sum
+
+      @scores_per_frame << total_score_this_frame
+
+      puts "Score: #{total_score_this_frame}"
+    end
   end
 end
 
